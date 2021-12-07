5cfecefb751bb0c55872c103bfa8bf9d9978276b
GitHub Action <action@github.com>
2021-12-07 10:51:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/philippgatzkaswt/startwithgit/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.philippgatzkaswt.startwithgit.services
+
+import com.github.philippgatzkaswt.startwithgit.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
